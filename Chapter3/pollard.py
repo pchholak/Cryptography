81f98b596006342133c4036693bfda2e947f1ebb
pchholak <parthchholak@gmail.com>
2020-03-11 15:51:59 +0100

    finished chap 3

--- Chapter3/pollard.py
+++ Chapter3/pollard.py
@@ -1,5 +1,6 @@
 from random import randint
 from math import gcd
+import math
 
 # Sieve of Eratosthenes
 def primes(n):
@@ -11,6 +12,25 @@ def primes(n):
             for i in range(p, n + 1, p):
                 b[i] = False
     return ps
+
+# Modular inverse
+def modular_inv(a, b):
+    if b==0:
+        v = 0
+        return v
+    else:
+        u, d = 1, a
+        v1, v3 = 0, b
+        while v3 != 0:
+            q, t3 = math.floor(d / v3), d % v3
+            t1 = u - q * v1
+            u = v1
+            d = v3
+            v1 = t1
+            v3 = t3
+        v = (d - a * u) / b
+        return v
+    return None
 
 # Square-and-multiply algorithm for exponentiation
 def modular_pow(x, d, n):
@@ -45,6 +65,32 @@ def pollard_rho(n):
         return pollard_rho(n)
     else:
         return g
+    return None
+
+# Step function for Pollard's rho for DLP
+def f(xab, g, h, p):
+    x, a, b = xab[0], xab[1], xab[2]
+    if x%3 == 0:
+        return [x*h%p, (a+1)%(p-1), b]
+    elif x%3 == 1:
+        return [x*x%p, a*2%(p-1), b*2%(p - 1)]
+    else:
+        return [x*g%p, a, (b+1)%(p-1)]
+    return None
+
+# Pollard's rho for DLP
+def pollard_rho_dl(g, h, p):
+    xab = 1, 0, 0
+    XAB = f(xab, g, h, p)
+    i = 1
+    while xab[0] != XAB[0]:
+        i, xab, XAB = i+1, f(xab,g,h,p), f(f(XAB,g,h,p),g,h,p)
+    d = gcd(xab[1]-XAB[1], p-1)
+    if d == 1: return ((XAB[2]-xab[2])*modular_inv(xab[1]-XAB[1], p-1)) % (p-1)
+    m, l = 0, int(((XAB[2]-xab[2])*modular_inv(xab[1]-XAB[1],(p-1)/d))%((p-1)/d))
+    while m <= d:
+        if modular_pow(g,l,p) == h%p: return l
+        m, l = m+1, int((l+((p-1)/d))%(p-1))
     return None
 
 # Pollard's p-1
